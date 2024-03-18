b3cc7159cc1600aea73a9f308998c65deeb39c22
jamesp918 <jamesp@botlet.io>
2024-03-19 00:33:24 +0800

    fix: canonical url host
    Signed-off-by: jamesp918 <jamesp@botlet.io>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: process.env.SITE_URL,
+  url: process.env.SITE_URL_DOCUMENTATION_URL,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.SITE_BASE_PATH,
